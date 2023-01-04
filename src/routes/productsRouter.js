4764a8c03231fc75432d823143600fe70915cf37
BadKernel <tonifenoy78@gmail.com>
2023-01-04 20:29:41 +0100

    peticiones get trabajando

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -9,4 +9,12 @@ router.delete("/delete_product/:id", productosCtrl.delete_product);
 router.get("/get_product/:id" ,productosCtrl.get_product);
 router.put("/edit_product/:id" ,productosCtrl.edit_product);
 
+// Routes for store
+router.get("/get_productType/:type", productosCtrl.get_productType);
+router.get("/get_productBrand/:brand", productosCtrl.get_productBrand);
+router.get("/get_productGraduation/:graduation", productosCtrl.get_productGraduation);
+router.get("/get_productScore/:score", productosCtrl.get_productScore);
+router.get("/get_productPrice/:price", productosCtrl.get_productPrice);
+
+
 module.exports = router;
--- src/services/usersModel.js
+++ src/services/usersModel.js
@@ -1,20 +1,20 @@
 const User = require('../models/userSchema')
 
 exports.get_users  = async () => {
-    return await User.find();
+  return await User.find();
 }
 
 exports.delete_user  = async (id) => {
-    return await User.findByIdAndDelete(id);
+  return await User.findByIdAndDelete(id);
 }
 
 exports.add_user  = async (body) => {
-   const user = new User(body);
-   return await User.create(user); 
+  const user = new User(body);
+  return await User.create(user); 
 }
 
 exports.get_user  = async (id) => {
-     return await User.findOne({ _id: id });
+  return await User.findOne({ _id: id });
 }
 
 exports.edit_user = async (id,body) => {
