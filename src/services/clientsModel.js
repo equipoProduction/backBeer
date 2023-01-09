78ca28d34be74e173dffffec39b2ce92d5b01cd9
BadKernel <tonifenoy78@gmail.com>
2023-01-09 16:38:19 +0100

    probado servidor badkernel

--- src/services/clientsModel.js
+++ src/services/clientsModel.js
@@ -1,20 +1,20 @@
 const Client = require('../models/clientShema')
 
 exports.get_clients  = async () => {
-    return await Client.find();
+  return await Client.find();
 }
 
 exports.delete_client  = async (id) => {
-    return await Client.findByIdAndDelete(id);
+  return await Client.findByIdAndDelete(id);
 }
 
 exports.add_client = async (body) => {
   const client = new Client(body);
-   return await client.save(); 
+  return await client.save(); 
 }
 
 exports.get_client  = async (id) => {
-     return await Client.findOne({ _id: id });
+  return await Client.findOne({ _id: id });
 }
 
 exports.edit_client = async (id,body) => {
--- src/services/ordersModel.js
+++ src/services/ordersModel.js
@@ -1,20 +1,20 @@
 const Order = require('../models/orderShema')
 
 exports.get_orders  = async () => {
-    return await Order.find();
+  return await Order.find();
 }
 
 exports.delete_order  = async (id) => {
-    return await Order.findByIdAndDelete(id);
+  return await Order.findByIdAndDelete(id);
 }
 
 exports.add_order = async (body) => {
   const order = new Order(body);
-   return await order.save(); 
+  return await order.save(); 
 }
 
 exports.get_order  = async (id) => {
-     return await Order.findOne({ _id: id });
+  return await Order.findOne({ _id: id });
 }
 
 exports.edit_order = async (id,body) => {
