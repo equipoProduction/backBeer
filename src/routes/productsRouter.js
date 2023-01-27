5cc917e43a37153b0dc602d3c2666d8f03419d97
BadKernel <106186643+DevBadKernel@users.noreply.github.com>
2023-01-27 15:58:13 +0100

    Pagestore (#7)
    * Peticiones homepage
    
    * corregido si producto pedido <0
    
    ---------
    
    Co-authored-by: Paula Sousa <paulasousa137@gmail.com>

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,14 +3,14 @@ const router = Router();
 const productosCtrl = require("../../src/controllers/productsController");
 
 // CRUD Admin for Routes Products
-// router.get("/get_products", productosCtrl.get_products);
+router.get("/get_products", productosCtrl.get_products);
 router.post("/add_product", productosCtrl.add_product);
 router.delete("/delete_product/:id", productosCtrl.delete_product);
 router.get("/get_product/:id" ,productosCtrl.get_product);
 router.put("/edit_product/:id" ,productosCtrl.edit_product);
 
 // Routes for home/store
-router.get("/get_productType/:type", productosCtrl.get_productType);
+router.get("/get_productCategory/:category", productosCtrl.get_productCategory);
 router.get("/get_productBrand/:brand", productosCtrl.get_productBrand);
 router.get("/get_productGraduation/:graduation", productosCtrl.get_productGraduation);
 router.get("/get_productScore/:score", productosCtrl.get_productScore);
